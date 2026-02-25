6e5ee4331ee5ed35849764a870c580c21cf9a428
Raaz <raazraz621@gmail.com>
2026-02-25 06:21:41 +0530

    updated

---
+++ python_dsa6.py
@@ -0,0 +1,20 @@
+#fixed size sliding window
+numbers = [2, 4, 3, 4, 1, 1]
+k = 3
+
+
+def max_sum_sub_array(nums, k):
+    current_sum = sum(nums[:k])
+    max_sum = current_sum
+
+    for i in range(k, len(nums)):
+        current_sum = current_sum - nums[i - k] + nums[i]
+        max_sum = max(max_sum, current_sum)
+    return max_sum
+
+print(max_sum_sub_array(numbers, k))
+    
+
+
+
+
