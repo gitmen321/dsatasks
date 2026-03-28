ffca94ce95aac77d4d6108543760ef3807d152bf
Raaz <raazraz621@gmail.com>
2026-03-28 22:18:39 +0530

    added

---
+++ script14.js
@@ -0,0 +1,19 @@
+//fibinocci series:
+
+function fib(n) {
+    if (n == 0) return 0;
+    if (n <= 2) return 1;
+
+    let prev = 0;
+    let curr = 1;
+    let next;
+
+    for (let i = 2; i <= n; i++) {
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
+}
+
+console.log(fib(5));
