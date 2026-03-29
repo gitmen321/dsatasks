a5f09fb2902b8cd590fb6d03e5414aba471f9671
Raaz <raazraz621@gmail.com>
2026-03-29 20:17:05 +0530

    `Updated script16.js with new palindrome checking functions and test cases`

--- script16.js
+++ script16.js
@@ -1,12 +1,27 @@
 
-
-function isPalindrome(word) {
-    const original = String(word).toLowerCase();
-
+function isPalindrome(str) {
+    const original = String(str).toLowerCase();
     const reversed = original.split('').reverse().join('');
 
     if (original !== reversed) return 'Not palindrome';
     return 'palindrome';
-}   
+}
 
-console.log(isPalindrome('malsayalam'));
+console.log(isPalindrome('malaweealam'));
+
+function isPalindromee(str) {
+    const word = String(str).toLowerCase();
+
+    let left = 0;
+    let right = word.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) return 'Not Palindrome'
+
+        left++;
+        right--;
+    }
+    return 'Palindrome';
+}
+
+console.log('RazaR:', isPalindromee('RasszaR'));
